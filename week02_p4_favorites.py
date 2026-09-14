2916f18bfa6f4c04a5965ec1f07b0b0e689b1b7a
zo3-iscool-CFA26 <zo3.iscool_CFA26@proton.me>
2026-09-14 15:52:02 -0500

    Delete week02_p4_favorites.py

--- week02_p4_favorites.py
+++
@@ -1,7 +0,0 @@
-color = "Blue"
-print(f"Favorite color: {color}")
-game = "Life is Strange 2"
-print(f"Favorite game: {game}")
-snack = "Peanut Butter crackers"
-print(f"Favorite snack: {snack}")
-print(f"Summary: {color}, {game}, {snack}")
